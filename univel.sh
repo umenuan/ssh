7fd662c7cb32775f139808dd3b4125317ee3585f
umenuan <umenuan@gmail.com>
2026-06-04 11:41:07 +0800

    Update univel.sh

--- univel.sh
+++ univel.sh
@@ -56,31 +56,16 @@ while true; do
             queue_algorithm=$(sysctl -n net.core.default_qdisc)
             os_info=$(lsb_release -ds 2>/dev/null)
             [ -z "$os_info" ] && os_info="Debian $(cat /etc/debian_version 2>/dev/null)"
-
-            output=$(awk 'BEGIN { rx_total = 0; tx_total = 0 }
-                NR > 2 { rx_total += $2; tx_total += $10 }
-                END {
-                    rx_units = "Bytes";
-                    tx_units = "Bytes";
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "KB"; }
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "MB"; }
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "GB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "KB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "MB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "GB"; }
-                    printf("总接收: %.2f %s\n总发送: %.2f %s\n", rx_total, rx_units, tx_total, tx_units);
-                }' /proc/net/dev)
-
+            net_traffic=$(awk 'NR>2{rx+=$2; tx+=$10} END {split("Bytes KB MB GB", u); while(rx>1024&&r<3){rx/=1024; r++}; while(tx>1024&&t<3){tx/=1024; t++}; printf "总接收: %.2f %s\n总发送: %.2f %s", rx, u[r+1], tx, u[t+1]}' /proc/net/dev)
             current_time=$(date "+%Y-%m-%d %I:%M %p")
             swap_used=$(free -m | awk 'NR==3{print $3}')
             swap_total=$(free -m | awk 'NR==3{print $2}')
             swap_percentage=0
             [ "$swap_total" -ne 0 ] && swap_percentage=$((swap_used * 100 / swap_total))
             swap_info="${swap_used}MB/${swap_total}MB (${swap_percentage}%)"
-            runtime=$(cat /proc/uptime | awk -F. '{run_days=int($1 / 86400);run_hours=int(($1 % 86400) / 3600);run_minutes=int(($1 % 3600) / 60); if (run_days > 0) printf("%d天 ", run_days); if (run_hours > 0) printf("%d小时 ", run_hours); if (run_minutes > 0) printf("%d分钟", run_minutes); }')
-
+            runtime=$(uptime -p)
             echo ""
-            echo -e "${white}系统信息详情${re}"
+            echo -e "${white}系统信息${re}"
             echo "------------------------"
             echo -e "${white}主机名: ${purple}${hostname}${re}"
             echo -e "${white}运营商: ${purple}${isp_info}${re}"
@@ -90,24 +75,24 @@ while true; do
             echo "------------------------"
             echo -e "${white}CPU架构: ${purple}${cpu_arch}${re}"
             echo -e "${white}CPU型号: ${purple}${cpu_info}${re}"
-            echo -e "${white}CPU核心数: ${purple}${cpu_cores}${re}"
+            echo -e "${white}CPU核心: ${purple}${cpu_cores}${re}"
             echo "------------------------"
             echo -e "${white}CPU占用: ${purple}${cpu_usage_percent}${re}"
             echo -e "${white}物理内存: ${purple}${mem_info}${re}"
             echo -e "${white}虚拟内存: ${purple}${swap_info}${re}"
             echo -e "${white}硬盘占用: ${purple}${disk_info}${re}"
             echo "------------------------"
-            echo -e "${purple}$output${re}"
+            echo -e "${purple}$net_traffic${re}"
             echo "------------------------"
             echo -e "${white}网络拥堵算法: ${purple}${congestion_algorithm} ${queue_algorithm}${re}"
             echo "------------------------"
-            echo -e "${white}公网IPv4地址: ${purple}${ipv4_address}${re}"
-            echo -e "${white}公网IPv6地址: ${purple}${ipv6_address}${re}"
+            echo -e "${white}IPv4地址: ${purple}${ipv4_address}${re}"
+            echo -e "${white}IPv6地址: ${purple}${ipv6_address}${re}"
             echo "------------------------"
             echo -e "${white}地理位置: ${purple}${country} $city${re}"
             echo -e "${white}系统时间: ${purple}${current_time}${re}"
             echo "------------------------"
-            echo -e "${white}系统运行时长: ${purple}${runtime}${re}"
+            echo -e "${white}运行时长: ${purple}${runtime}${re}"
             echo ""
             echo -e "${yellow}按任意键返回...${re}"
             read -n 1 -s -r -p ""
