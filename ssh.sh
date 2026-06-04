2e706eb5880b1060ddbfcde92cc2e5e54f067a4e
umenuan <umenuan@gmail.com>
2026-06-04 11:34:35 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -51,21 +51,7 @@ while true; do
             queue_algorithm=$(sysctl -n net.core.default_qdisc)
             os_info=$(lsb_release -ds 2>/dev/null)
             [ -z "$os_info" ] && os_info="Debian $(cat /etc/debian_version 2>/dev/null)"
-
-            output=$(awk 'BEGIN { rx_total = 0; tx_total = 0 }
-                NR > 2 { rx_total += $2; tx_total += $10 }
-                END {
-                    rx_units = "Bytes";
-                    tx_units = "Bytes";
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "KB"; }
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "MB"; }
-                    if (rx_total > 1024) { rx_total /= 1024; rx_units = "GB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "KB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "MB"; }
-                    if (tx_total > 1024) { tx_total /= 1024; tx_units = "GB"; }
-                    printf("总接收: %.2f %s\n总发送: %.2f %s\n", rx_total, rx_units, tx_total, tx_units);
-                }' /proc/net/dev)
-
+            net_traffic=$(awk 'NR>2{rx+=$2; tx+=$10} END {split("Bytes KB MB GB", u); while(rx>1024&&r<3){rx/=1024; r++}; while(tx>1024&&t<3){tx/=1024; t++}; printf "总接收: %.2f %s\n总发送: %.2f %s", rx, u[r+1], tx, u[t+1]}' /proc/net/dev)
             current_time=$(date "+%Y-%m-%d %I:%M %p")
             swap_used=$(free -m | awk 'NR==3{print $3}')
             swap_total=$(free -m | awk 'NR==3{print $2}')
@@ -92,7 +78,7 @@ while true; do
             echo -e "${white}虚拟内存: ${purple}${swap_info}${re}"
             echo -e "${white}硬盘占用: ${purple}${disk_info}${re}"
             echo "------------------------"
-            echo -e "${purple}$output${re}"
+            echo -e "${purple}$net_traffic${re}"
             echo "------------------------"
             echo -e "${white}网络拥堵算法: ${purple}${congestion_algorithm} ${queue_algorithm}${re}"
             echo "------------------------"
