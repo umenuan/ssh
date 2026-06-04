bb19d269b2aca95bd0bd766f6efd69dc824fb4bc
umenuan <umenuan@gmail.com>
2026-06-04 13:41:37 +0800

    Update univel.sh

--- univel.sh
+++ univel.sh
@@ -27,7 +27,7 @@ while true; do
             ipv4_address=$(curl -s -m 2 ipv4.ip.sb)
             ipv6_address=$(curl -s -m 2 ipv6.ip.sb)
             cpu_info=$(awk -F: '/model name/ {print $2; exit}' /proc/cpuinfo | sed 's/^ *//')
-            cpu_usage_percent=$(top -bn1 | awk '/Cpu\(s\)/ {printf "%.2f%%\n", 100 - $8}')
+            cpu_usage_percent=$(awk '{u=$2+$3+$4+$6+$7+$8;t=u+$5;if(NR==1){u1=u;t1=t}else printf"%.2f%%\n",(u-u1)*100/(t-t1)}' <(grep '^cpu ' /proc/stat) <(sleep 1;grep '^cpu ' /proc/stat))
             cpu_cores=$(nproc)
             mem_info=$(free -b | awk 'NR==2{u=$3/1048576;t=$2/1048576;printf"%.2f/%.2f MB (%.2f%%)",u,t,u*100/t}')
             disk_info=$(df -h / | awk 'NR==2{printf "%s/%s (%s)", $3, $2, $5}')
