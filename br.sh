d04bce03082e6efda481c942090cfb9c029304c6
umenuan <umenuan@gmail.com>
2026-06-05 13:18:52 +0800

    Delete br.sh

--- br.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "net.core.default_qdisc=fq" > /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-
-sysctl --system >/dev/null 2>&1
-
-sysctl net.ipv4.tcp_congestion_control net.core.default_qdisc
