51e5b9b2a95a4e62bf01b8d57e6ceb833999bd9c
umenuan <umenuan@gmail.com>
2026-06-06 21:16:03 +0800

    Update univel.sh

--- univel.sh
+++ univel.sh
@@ -86,7 +86,7 @@ while true; do
             echo -e "${yellow}正在更新...${re}"
             apt update && apt upgrade -y
             apt autoremove -y && apt autoclean -y
-            apt install -y curl wget unzip sudo fail2ban ufw openssl
+            apt install -y curl wget unzip sudo ufw openssl
             echo -e "${green}更新完成！${re}"
             read -n 1 -s -r -p ""
             ;;
