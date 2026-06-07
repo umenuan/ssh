eafc238866a83a3b3726ae5eff293be452b2c162
umenuan <umenuan@gmail.com>
2026-06-07 19:55:48 +0800

    Update opt.sh

--- opt.sh
+++ opt.sh
@@ -10,6 +10,7 @@ while true; do
     echo "5) 安装 3X-UI"
     echo "6) 安装 Speedtest"
     echo "7) 安装 1Panel"
+    echo "8) 切换 DNS"
     echo "0) 退出脚本"
     echo "================="
     read -p "Pick (1-5): " choice
@@ -35,6 +36,17 @@ while true; do
             ;;     
         7)
             bash -c "$(curl -sSL https://resource.fit2cloud.com/1panel/package/v2/quick_start.sh)"           
+            ;;
+        8)
+            echo -e "1) Cloudflare\n2) Google"
+            read -rp "Pick [1-2]: " c
+            case $c in
+            1) dns="1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";;
+            2) dns="8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844";;
+            *) exit 1;;
+            esac
+            : >/etc/resolv.conf; for i in $dns; do echo "nameserver $i" >> /etc/resolv.conf; done
+            cat /etc/resolv.conf          
             ;;
         0)
             echo "退出脚本..."
