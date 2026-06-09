ad6f5d08df616e4df9c6c9b17d6495e7cf50a7cc
umenuan <umenuan@gmail.com>
2026-06-09 16:56:24 +0800

    Update opt.sh

--- opt.sh
+++ opt.sh
@@ -11,7 +11,7 @@ while true; do
     echo "6) 安装 Speedtest"
     echo "7) 安装 1Panel"
     echo "8) 安装 Docker"
-    echo "9) 切换 DNS"
+    echo "9) 安装 Debian13"
     echo "0) 退出脚本"
     echo "================="
     read -p "Pick (1-9): " choice
@@ -42,15 +42,8 @@ while true; do
             curl -fsSL https://get.docker.com | sudo sh           
             ;;
         9)
-            echo -e "1) Cloudflare\n2) Google"
-            read -rp "Pick [1-2]: " c
-            case $c in
-            1) dns="1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";;
-            2) dns="8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844";;
-            *) exit 1;;
-            esac
-            : >/etc/resolv.conf; for i in $dns; do echo "nameserver $i" >> /etc/resolv.conf; done
-            cat /etc/resolv.conf          
+            clear
+            bash <(curl -Ls https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh) debian 13
             ;;
         0)
             echo "退出脚本..."
