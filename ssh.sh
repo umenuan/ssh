9a7b0297ae510cef13b6626de5291b678aab92dd
umenuan <umenuan@gmail.com>
2026-09-01 21:38:03 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -60,7 +60,7 @@ while true; do
             printf "${white}%-12s${purple}%s${re}\n" "IPv6:"     "${ipv6}"
             echo -e "$line"
             printf "${white}%-12s${purple}%s${re}\n" "DNS:" "${dns_list[0]}"
-            printf "${white}%-12s${purple}%s${re}\n" "" "${dns_list[@]:1}"
+            for d in "${dns_list[@]:1}"; do printf "${white}%-12s${purple}%s${re}\n" "" "$d"; done
             echo -e "$line"
             read -n 1 -s -r -p ""
             echo ""
